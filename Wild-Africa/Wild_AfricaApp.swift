24d290411c186c856b35a5a3a652f1bc1a53d3c9
Jigar Dave <jdavenz8@gmail.com>
2021-04-28 15:18:47 +0530

    Initial Commit

---
+++ Wild-Africa/Wild_AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wild_AfricaApp.swift
+//  Wild-Africa
+//
+//  Created by jigar dave on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Wild_AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
